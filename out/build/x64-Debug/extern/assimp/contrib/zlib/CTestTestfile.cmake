d42fb37bac4460c29cb5770d5f1bd0af87345df6
Tomoya Kozuki <7544kozuki@gmail.com>
2025-12-05 19:53:24 +0900

    CMakeLists.txtの名前を修正

---
+++ out/build/x64-Debug/extern/assimp/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/7544k/Github/maindir/extern/assimp/contrib/zlib
+# Build directory: C:/Users/7544k/Github/maindir/out/build/x64-Debug/extern/assimp/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
